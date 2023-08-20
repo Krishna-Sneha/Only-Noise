a1c93483cba7bf91739a2fdfefb4f722dc89cb5e
S Krishna Sneha <87603454+Krishna-Sneha@users.noreply.github.com>
2023-08-20 22:48:02 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -27,21 +27,9 @@ app.use(
 
 const PORT = process.env.PORT;
 
-// ---------------------deployment-----------------------
-// const __dirname1 = path.resolve();
-// if (process.env.NODE_ENV === "production") {
-//   console.log("inside production");
-//   app.use(express.static(path.join(__dirname1, "/frontend/dist")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"));
-//   });
-// } else
-{
   app.get("/", (req, res) => {
     res.send("API running successfully!");
   });
-}
-// ---------------------deployment-----------------------
 
 app.use(express.json()); //to accept JSON data recieving from frontend
 
